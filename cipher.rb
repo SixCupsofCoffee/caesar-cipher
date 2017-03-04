c6698e591a2e8835d4a84f587352a683ebf6d1b2
John Bell <ja.bell@me.com>
2017-03-04 15:20:45 -0500

    create cipher and test with phrases

--- cipher.rb
+++ cipher.rb
@@ -1,33 +1,8 @@
-def caesar_cipher(string, shift)
-  arr = string.split(//)
-  arr.map! do |n|
-    if n.ord.between?(65,90)
-      x = n.ord + shift
-      if x > 90
-        z = x - 90
-        x = z + 65
-        n = x.chr
-      else
-        n = x.chr
-      end
-    elsif n.ord.between?(97,122)
-      x = n.ord + shift
-      if x > 122
-        z = x - 122
-        x = z + 97
-        n = x.chr
-      else
-        n = x.chr
-      end
-    else
-      x = n.ord
-      n = x.chr
-    end
-  end
+def cipher(string, n)
+	string.split('').map { |c|
+		((65..90) === c.ord) ? (( c.ord + n - 65 ) % 26 + 65).chr : ((97..122) === c.ord) ? (( c.ord + n - 97 ) % 26 + 97).chr : c
+	}.join('')
+end
 
-  string = arr.join
-  puts string
-endgit
-
-caesar_cipher("Test String!", 3)
-caesar_cipher("Hail Caesar", 7)
+puts cipher("Test String!", 5)
+puts cipher("Hail Caesar", 7)
